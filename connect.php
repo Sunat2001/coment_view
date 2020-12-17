f4092e3351bbdf8cdba38f881c8bb0d32a7e03a6
Sunat2001 <76121600+Sunat2001@users.noreply.github.com>
2020-12-17 11:09:47 +0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -12,7 +12,7 @@ $db_database		= 'ca68017_coments';
 
 $link = @mysqli_connect($db_host,$db_user,$db_pass,$db_database) or die('Не могу установить соединение с базой данных');
 
-mysqli_query($link,"SET NAMES 'utf-8'");
+mysqli_query($link,"SET NAMES 'utf8'");
 
 
 ?>
